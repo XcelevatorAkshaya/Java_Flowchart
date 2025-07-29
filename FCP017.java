7cef0aa95196a776902dfef52da3dffc5d07b360
XcelevatorAkshaya <akshaya.2503@xcelevate.org>
2025-07-30 00:55:15 +0530

    FCP017

---
+++ FCP017.java
@@ -0,0 +1,18 @@
+public class FCP017{
+public static void main(String[]args){
+int a=5;
+int b=10;
+int c=15;
+if(a>b){
+if(a<c){
+System.out.println(a);
+}
+}
+else if(b>c){
+System.out.println(c);
+}
+else{
+System.out.println(b);
+}
+}
+}

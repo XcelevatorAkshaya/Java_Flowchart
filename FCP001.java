fc2c258b491e0f15699e666a24697c6e457ec82e
XcelevatorAkshaya <akshaya.2503@xcelevate.org>
2025-07-29 23:54:50 +0530

    FCP001

---
+++ FCP001.java
@@ -0,0 +1,12 @@
+public class FCP001{
+public static void main(String[]args){
+int birth_date=05;
+String birth_month="July";
+int birth_year=2003;
+System.out.println(birth_date);
+System.out.println(birth_month);
+System.out.println(birth_year);
+}
+}
+
+

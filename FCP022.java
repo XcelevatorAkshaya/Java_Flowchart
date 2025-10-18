417366dfdf9e08d72f0475a8e7b04e5f83327a5b
XcelevatorAkshaya <akshaya.2503@xcelevate.org>
2025-10-18 15:55:15 +0530

    fcp022

---
+++ FCP022.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class FCP026 {
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+int d=sc.nextInt();
+int e=sc.nextInt();
+int x;
+if(a==b && a==c && a==d && a==e){
+x=5;
+System.out.println("a"=x);
+}
+if(b|
+
